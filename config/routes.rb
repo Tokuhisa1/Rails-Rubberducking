37412c3098d6dcde7a4a1fc30484d71955c28928
Winston Tokuhisa <TokuhisaWinston@outlook.com>
2018-08-17 09:56:11 -0400

    Adds comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # Create a route for each of the necessary actions - no more, no less
   resources :ducks, only: [:index, :show, :new, :create, :edit, :update]
   resources :students, only: [:index, :show, :new, :create, :edit, :update]
 end
